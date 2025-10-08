59cdba79869ccfdd57dc52532e856208276f47aa
AdrianoII <adrianocorbelinoii@gmail.com>
2025-10-08 17:16:59 -0400

    Fix navbar under copy page

--- app/components/navbar.tsx
+++ app/components/navbar.tsx
@@ -5,10 +5,10 @@ import { usePathname } from 'next/navigation'
 export default function NavBar() {
   const pathname = usePathname()
   return (
-    <nav className="flex items-center justify-center gap-[16px]">
-      <div role="tablist" className="tabs  tabs-lift tabs-xl">
+    <nav className="mt-4 text-accent flex items-center justify-center">
+      <div role="tablist" className="tabs tabs-lift tabs-xl">
         <Link href="/" role="tab" className={`tab ${pathname === "/" ? "tab-active" : ""}`}>About</Link>
-        <Link href="/projects" role="tab" className={`tab ${pathname === "/projects" ? "tab-active" : ""}`}>Projects</Link>
+        <Link href="/projects" role="tab" className={`tab ${pathname.startsWith("/projects") ? "tab-active" : ""}`}>Projects</Link>
         <Link href="/cv" role="tab" className={`tab ${pathname === "/cv" ? "tab-active" : ""}`}>CV</Link>
       </div>
     </nav>
