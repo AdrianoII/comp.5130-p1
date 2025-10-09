2c2e27d3b0e7a41b9da390e88956250c274b0169
AdrianoII <adrianocorbelinoii@gmail.com>
2025-10-08 22:26:57 -0400

    Add title to iframe in /cv

--- app/cv/page.tsx
+++ app/cv/page.tsx
@@ -6,7 +6,7 @@ export default function CV() {
             <h1 className="prose text-accent prose-md max-w-none text-center w-full text-xl lg:text-3xl basis-1/5">📃 You can find my CV in the reader below, but also <a href="https://github.com/AdrianoII/adrianocorbelinoii.com/blob/main/public/Resume_EN.pdf">here</a>.</h1>
             <div className="basis-4/5">
                 <iframe src={`https://docs.google.com/gview?url=${"https://www.adrianocorbelinoii.com/Resume_EN.pdf"}&embedded=true`}
-                    className="w-full min-h-[70vh]" />
+                    className="w-full min-h-[70vh]" title="PDF reader for Adriano's CV" />
             </div>
         </section>
     );
